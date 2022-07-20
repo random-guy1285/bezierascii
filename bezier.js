2e38c67cc3c5badea77de7ad8622e608422a0978
Random guy <88682110+random-guy1285@users.noreply.github.com>
2022-07-21 00:33:12 +0300

    Update bezier.js

--- bezier.js
+++ bezier.js
@@ -30,14 +30,11 @@ let arr4 = ['',''];
  arr3[1] = Number(arr3[1])
  arr4[1] = Number(arr4[1])
 console.log(arr1,arr2,arr3,arr4)
-//pretty self explanatory
 const pyth = (x1,y1,x2,y2) => {
 const x = Math.abs(x1-x2)**2;
 const y = Math.abs(y1-y2)**2;
 return Math.sqrt(x+y)
 }
-/*should return a point that is t (0-1) of the way from point 1 to 2
-i think it works but im not sure*/
 const tsolve = (arr=[0,0],arr2=[0,0],t=0) => {
 const ang = Math.atan2(arr[1]-arr2[1],arr[0]-arr2[0])+Math.PI;
 const dst = pyth(...arr,...arr2);
@@ -45,14 +42,6 @@ const x = arr[0]+(Math.cos(ang)*(dst*t));
 const y = arr[1]+(Math.sin(ang)*(dst*t));
 return [x,y]
 }
-
-//setup
-/*
-const arr1 = [0,0];
-const arr2 = [40,0];
-const arr3 = [0,40];
-const arr4 = [40,40];
-*/
 const x = [];
 for(let i = 0;i<=40;i++){x.push(['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'])};
 
@@ -65,7 +54,6 @@ const d4 = tsolve(d1,d2,t);
 const d5 = tsolve(d2,d3,t);
 const d6 = tsolve(d4,d5,t);
 const end = [Math.round(d6[0]),Math.round(d6[1])];
-console.log('\nend ',end)
 x[end[0]][end[1]] = '#'
 }
 for(let i = 0;i<40;i++){
